af83e14abb8c961497aa171f5f43b9d9cd46c711
Maloice <maloice18@gmail.com>
2026-02-22 20:36:06 +0800

    add base app id metatag

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ export async function generateMetadata(): Promise<Metadata> {
     title: farcasterConfig.miniapp.name,
     description: farcasterConfig.miniapp.description,
     other: {
+      "base:app_id": farcasterConfig.miniapp.homeUrl,
       "fc:frame": JSON.stringify({
         version: farcasterConfig.miniapp.version,
         imageUrl: farcasterConfig.miniapp.heroImageUrl,
